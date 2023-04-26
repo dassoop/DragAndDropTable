1233427348d7e2f3629b7a15ead1b7d2968c0f66
dassoop <ssummer.lance@gmail.com>
2023-04-26 12:40:29 -0700

    switch to jquery traversal

--- script.js
+++ script.js
@@ -5,16 +5,12 @@ function allowDrop(ev)
 
 function drag(ev) 
 {
-    // console.log('id: ' + ev.target.id)
-    let id = ev.target.id.replace('drag-', '')
-    ev.dataTransfer.setData("id", id);
+    ev.dataTransfer.setData("id", '#' + ev.currentTarget.id);
 }
 
 function dragEnd(ev)
 {
-    // console.log('dragEnd')
-    // const element = ev.currentTarget;
-    // element.style.borderRadius = '0px';
+
 }
 
 function drop(ev) 
@@ -23,49 +19,43 @@ function drop(ev)
     $('.note-display').each(function(){
         $(this).css('background-color', 'white')
     })
-    let newValue = $(ev.target).attr('data-number')
-    var id = '#note-control-select-' + ev.dataTransfer.getData("id");
-    // console.log('oldID: ' + id)
-    // console.log(selectedItem.find(":selected").text())
-    $(id).val(newValue)
+    let newValue = $(ev.currentTarget).attr('data-number')
+    $(ev.dataTransfer.getData('id')).find('select').val(newValue)
 }
 
 function hover(ev)
 {
-    let id = ev.currentTarget.id.replace('drag-', '#note-control-select-')
-    // console.log('Enter: ' + $(id).val())
-    let noteDisplay = '#note-display-' + $(id).val()
-    $(noteDisplay).css('background-color', '#d0acd0')
-    $(noteDisplay).css('border-radius', '5px')
+    let noteDisplay = '#note-display-' + $('#' + ev.currentTarget.id).find('select').val()
+    $(noteDisplay).css({
+        'background-color': '#d0acd0',
+        'border-radius': '5px',
+        'padding': '3px'
+    })
 }
 
 function leave(ev)
 {
-    let id = ev.currentTarget.id.replace('drag-', '#note-control-select-')
-    // console.log('Leave: ' + $(id).val())
-    let noteDisplay = '#note-display-' + $(id).val()
+    let noteDisplay = '#note-display-' + $('#' + ev.currentTarget.id).find('select').val()
     $(noteDisplay).css('background-color', 'white')
 }
 
 function reset(ev)
 {
-    let targetId = ev.currentTarget.id
-    // console.log(targetId)
-
-    let newId = targetId.replace('btn-reset-', '#note-control-select-')
-    $(newId).val($(newId).attr('data-defaultValue'))
+    let select = $('#' + ev.currentTarget.id).closest('span').find('div').find('select')
+    select.val(select.attr('data-defaultValue'))
 }
 
 function changeNote(ev)
 {
     $('.note-display').css('background-color', 'white')
-    let id = '#' + ev.currentTarget.id
-    // console.log('Enter: ' + $(id).val())
+    let id = ev.currentTarget.id
     let noteDisplay = '#note-display-' + $(id).val()
-    $(noteDisplay).css('background-color', '#d0acd0')
-    $(noteDisplay).css('border-radius', '5px')
+    $(noteDisplay).css({
+        'background-color': '#d0acd0',
+        'border-radius': '5px',
+        'padding': '3px'
+    })
 }
-
 function highlightBackground()
 {
     //function to unify highlight and de-highlight backgrounds
@@ -73,9 +63,7 @@ function highlightBackground()
 
 function resetAllNotes(ev)
 {
-    // console.log('reset all notes')
     $('.note-control-select').each(function(){
         $(this).val($(this).attr('data-defaultValue'))
-        // console.log($(this).attr('data-defaultValue'))
     })
 }
